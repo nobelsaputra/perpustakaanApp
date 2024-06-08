ff61d68452467bef294097e4a3359c79ae727e93
nobelsaputra53 <150402008+nobelsaputra@users.noreply.github.com>
2024-06-08 11:09:47 +0800

    deploy to vercell

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000; 
 
-const routes = require('./routes');
+const routes = require('../routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,3 +13,5 @@ app.use(routes);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
